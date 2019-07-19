872b323eeec8b66220c80faec5c346f27c14dd1b
Mourad <saidilive@hotmail.com>
2019-07-19 20:41:34 +0200

    Mise à jours

---
+++ src/Entity/Fonction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\FonctionRepository")
+ */
+class Fonction
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Fonction;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFonction(): ?string
+    {
+        return $this->Fonction;
+    }
+
+    public function setFonction(string $Fonction): self
+    {
+        $this->Fonction = $Fonction;
+
+        return $this;
+    }
+}
